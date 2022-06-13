5ae13305b4fd59ea519b7d054d151ec778a46707
anastasiyakhom <anastasiya.khomyk@allianzdirect.es>
2022-06-13 13:00:48 +0200

    first changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appsplit';
-}
+  serverElement = [{type: "server",name:"Testserver", content: "Just a test!"}];
+  }
+
